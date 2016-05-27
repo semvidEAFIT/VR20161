c2fda403019faf1cec959394e5f32729cb1b280b
diegoxfx <diegoelmalo2@gmail.com>
2016-05-27 09:06:55 -0500

    Jetpack, sólo se mueve vertical

---
+++ VR explorer/Assets/Scripts/JetPack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JetPack : MonoBehaviour
+{
+    float speed = 5.0f;
+    Vector3 moveDir = Vector3.zero;
+    float gravity = -9.8f;
+    public float thrust;
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") == 1)
+        {
+            rb.AddForce(transform.up * 50);
+        }
+        transform.Translate(moveDir);
+    }
+}
